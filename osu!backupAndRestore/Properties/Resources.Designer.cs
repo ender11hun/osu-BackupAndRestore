f9c10e2db2fc184dd63349dfaee6317ba58182d8
ender11hun <ender11hun@github.com>
2019-06-29 21:57:02 +0200

    Removed old icon record from the Resources.resx (yeah, don't say a word, I'm already searching for that rope)

--- osu!backupAndRestore/Properties/Resources.Designer.cs
+++ osu!backupAndRestore/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace osu_backupAndRestore.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon icon01 {
-            get {
-                object obj = ResourceManager.GetObject("icon01", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
     }
 }
