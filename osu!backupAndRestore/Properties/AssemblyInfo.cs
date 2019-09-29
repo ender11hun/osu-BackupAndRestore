58e7024060a7295781bb4a1bf8e7479679991ae9
ender11hun <ender11hun@github.com>
2019-09-29 20:12:38 +0200

    Updated version number (yes, a separate commit for this, don't ask why)

--- osu!backupAndRestore/Properties/AssemblyInfo.cs
+++ osu!backupAndRestore/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c3f9fa01-d172-4f51-9804-52735796ae5a")]
 
 [assembly: AssemblyVersion("1.9.*")]
-[assembly: AssemblyFileVersion("1.9.7.2")]
+[assembly: AssemblyFileVersion("1.9.7.3")]
 // Suffixes
 // T - Test version*
 // A - Alpha
