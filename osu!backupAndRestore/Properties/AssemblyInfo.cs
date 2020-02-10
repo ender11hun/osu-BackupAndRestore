b8eb870806b3997c4c7d0947e1b815369a544fe0
Ender S Gergo <ender11gomba@gmail.com>
2020-02-10 17:12:16 +0100

    Version bump

--- osu!backupAndRestore/Properties/AssemblyInfo.cs
+++ osu!backupAndRestore/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c3f9fa01-d172-4f51-9804-52735796ae5a")]
 
 [assembly: AssemblyVersion("1.10.*")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.1.0")]
 // Suffixes
 // T - Test version*
 // A - Alpha
